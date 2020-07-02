161d2cd96b733aa5ed52086dba434e29424cc749
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-07-02 22:45:38 +0430

    Update index.php

--- index.php
+++ index.php
@@ -8,5 +8,6 @@ $googledrive = "https://drive.google.com/uc?export=download&id=";
 $googledriveid = $_GET['url'];
 ?>
 <iframe style="display:none" src="<?= $googledrive.$googledriveid ?>"></iframe>
+<script> document.title = "تبلیغات"; </script>
 </body>
 </HTML>
